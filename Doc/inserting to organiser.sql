1aebe34303660bd30b99e2b4ef853b7d5bc1863d
ebbyebenezar <makhubele8louis9@gmail.com>
2026-09-04 19:33:57 +0200

    Delete Doc directory

--- Doc/inserting to organiser.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Organiser
-    (OrganiserName, Email, PhoneNumber)
-VALUES
-    ('Run South Africa', 'info@runsouthafrica.co.za', '0111234567'),
-    ('Mzansi Road Events', 'events@mzansiroad.co.za', '0217654321');
