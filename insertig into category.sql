0f15c686288d39647de7e277d994da2e4127b282
ebbyebenezar <makhubele8louis9@gmail.com>
2026-09-04 20:35:43 +0200

    inserting into category

---
+++ insertig into category.sql
@@ -0,0 +1,14 @@
+INSERT INTO Category
+    (EventID, CategoryName, Distance, EntryFee)
+VALUES
+    -- Johannesburg City Run
+    (1, '10 KM Run', 10.00, 150.00),
+    (1, '21 KM Half Marathon', 21.10, 250.00),
+
+    -- Soweto Community Walk
+    (2, '5 KM Walk', 5.00, 80.00),
+    (2, '10 KM Walk', 10.00, 120.00),
+
+    -- Cape Town Cycle Challenge
+    (3, '40 KM Cycle', 40.00, 300.00),
+    (3, '80 KM Cycle', 80.00, 450.00);
