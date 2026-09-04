597f7b1d6167aae1387b628a3764d5784e17b469
ebbyebenezar <makhubele8louis9@gmail.com>
2026-09-04 19:09:23 +0200

    creating a table for partisipants

--- Doc/partisipants.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Participant
-(
-    ParticipantID INT IDENTITY(1,1) PRIMARY KEY,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    PhoneNumber VARCHAR(20) NOT NULL,
-    DateOfBirth DATE NOT NULL
-);
